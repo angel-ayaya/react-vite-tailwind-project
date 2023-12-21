2b746cc2994e619efff49734098df5c6d3677a1e
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-12-21 12:04:47 -0700

    Enviroment Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black'>
+      <h1 className="text-3xl font-bold text-white">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
