22b11efbf54557420b0a18eb212bfdd556be8511
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-12-21 19:00:28 -0700

    Refactor Card component and fetch data in Home component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,38 +1,40 @@
-const Card = () => {
+const Card = (props) => {
+  const { data } = props;
+  console.log(data);
+
   return (
-    <div>
-      <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-        <a
-          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-          href="#"
-        >
-          <img
-            className="object-cover hover:scale-105 hover:blur-sm hover:brightness-98 transition-all duration-500 ease-in-out"
-            src="https://images.pexels.com/photos/1070360/pexels-photo-1070360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1    "
-            alt="product image"
-          />
-          <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
-            Categoria
-          </span>
+    <div className="relative m-10 flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+      <a
+        className="relative mx-3 mt-3 flex h-56 overflow-hidden rounded-xl"
+        href="#"
+      >
+        <img
+          className="object-cover hover:scale-105 hover:blur-sm hover:brightness-98 transition-all duration-500 ease-in-out"
+          src="https://images.pexels.com/photos/1070360/pexels-photo-1070360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="product image"
+        />
+        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+          {data.category.name}
+        </span>
+      </a>
+      <div className="mt-4 px-5 pb-5">
+        <a href="#">
+          <h5 className="font-bold text-xl tracking-tight text-slate-900">
+            Nike Air MX Super
+          </h5>
         </a>
-        <div className="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 className="text-xl tracking-tight text-slate-900">
-              Nike Air MX Super 2500 - Red
-            </h5>
-          </a>
-          <div className="mt-2 mb-5 flex items-center justify-between">
-            <p>
-              <span className="text-3xl font-bold text-slate-900">$449</span>
-            </p>
-          </div>
+        <div className="mt-2  flex items-center justify-between">
+          <p>
+            <span className="text-2xl font-bold text-slate-900">$449</span>
+          </p>
           <a
-            href="#"
-            className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
-          >
-            Add to cart
-          </a>
+          href="#"
+          className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          Add to cart
+        </a>
         </div>
+        
       </div>
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,31 @@
-import Layout from "../../Components/Layout"
-import Card from "../../Components/Card"
+import { useState, useEffect } from "react";
+
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState([]);
 
-    return (
-      <Layout>
-        <Card/>
-      </Layout>
-    )
-  }
-  
-  export default Home
-  
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data)
+        
+      });
+      
+  }, []);
+  return (
+    <Layout>
+      Home
+      {
+        items?.map((item) => (
+          <Card key={item.id} data={item} />
+          
+        ))
+      } 
+    </Layout>
+  )
+}
+
+export default Home;
