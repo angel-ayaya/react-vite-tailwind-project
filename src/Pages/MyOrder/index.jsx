f59c6523551790557956de110c803b1814121fc0
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-12-27 12:45:29 -0700

    Add navigation and styling to MyOrder page

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,20 +2,29 @@ import Layout from "../../Components/Layout";
 import CartCard from "../../Components/CartCard";
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
+import { Link } from "react-router-dom";
+
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext);
-
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  if (index === "last") {
+    index = context.order.length - 1;
+  }
   const content = context.order
-    ?.slice(-1)[0]
+    ?.[index]
     ?.products?.map((product) => (
       <CartCard key={product.id} product={product} />
     )) || (
     <div className="text-wrap mt-12">
-      <h1 className="text-6xl	">You don't have any order yet.</h1>
+      <h1 className="text-6xl">You don&apos;t have any order yet.</h1>
       <p className="mt-12 text-2xl">
         Please go to the
-        <a href="/" className="font-bold"> Home page </a>
+        <a href="/" className="font-bold">
+          {" "}Home page{" "}
+        </a>
         and add some products to your cart.
       </p>
     </div>
@@ -23,9 +32,13 @@ function MyOrder() {
 
   return (
     <Layout>
-      <div className="flex flex-col">
-        {content}
+      <div className="flex items-center justify-center relative w-80 h-8 my-6">
+        <Link to="/my-orders" className="h-full absolute left-0">
+        <ChevronLeftIcon className="h-full  text-black cursor-pointer" />
+        </Link>
+          <h1 className="text-2xl">My orders</h1>
       </div>
+      <div className="flex flex-col">{content}</div>
     </Layout>
   );
 }
