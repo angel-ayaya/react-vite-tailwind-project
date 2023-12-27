ac1b89a8b558cb420908be69542239ba91d282f3
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-12-26 18:53:25 -0700

    Refactor CartCard and CheckoutSideMenu components

--- src/Components/CartCard/index.jsx
+++ src/Components/CartCard/index.jsx
@@ -1,26 +1,36 @@
+import PropTypes from 'prop-types';
 import { XCircleIcon } from '@heroicons/react/24/outline'
 
 
-const CartCard = ({product}) => {
-    console.log();
 
+
+const CartCard = ({product, handleDelete}) => {
+    let renderXCircleIcon 
+    if (handleDelete){
+        renderXCircleIcon = <XCircleIcon onClick={() => handleDelete(product.id)} className='w-6 h-6 cursor-pointer' />
+    }
     return (
         <div>
-            <div className='flex justify-between items-center p-6'>
+            <div className='flex justify-between items-center p-6 hover:shadow-inner hover:bg-slate-100'>
                 <div className='flex items-center cursor-pointer'>
                     <img src={product.images} alt={product.title} className='w-16 h-16 object-cover rounded-md' />
                     <div className='ml-4'>
                         <h3 className='font-medium text-lg'>{product.title}</h3>
-                        <p className='text-gray-500 text-sm'>{product.price}</p>
+                        <p className='text-gray-500 text-sm'>${product.price}</p>
                     </div>
                 </div>
                 <div>
-                    <XCircleIcon className='w-6 h-6 cursor-pointer' />
+                    {renderXCircleIcon}
                 </div>
             </div>
         </div>
     );
 
 }
+
+// PropType is a library for typechecking of props in react
+CartCard.propTypes = {
+    product: PropTypes.object.isRequired
+  }
 
 export default CartCard;
--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,32 +1,78 @@
-import './styles.css'
-import { XCircleIcon } from '@heroicons/react/24/outline'
-import { useContext } from 'react'
-import { ShoppingCartContext } from '../../Context'
-import CartCard from '../CartCard'
+import "./styles.css";
+import { Link } from "react-router-dom";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import CartCard from "../CartCard";
+import { totalPrice } from "../../utils";
 
 const CheckoutSideMenu = () => {
-    const context = useContext(ShoppingCartContext);
-    return (
-        <aside
-            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu overflow-y-auto flex flex-col fixed right-0 bg-white border-l shadow-2xl border-slate-100 shadow-gray-800`}>
-            <div className='flex justify-between items-center p-6'>
-                <h2 className='font-medium text-xl'>My Order</h2>
-                <div>
-                    <XCircleIcon className='w-6 h-6 cursor-pointer'
-                        onClick={() => context.closeCheckoutSideMenu()}
-                    />
-                </div>
+  const context = useContext(ShoppingCartContext);
 
-            </div>
-            <div>
-                
-                {context.carProducts.map((product) => (
-                    <CartCard product={product} />
-                ))}
-            </div>
-            
-        </aside>
+  const handleDelete = (id) => {
+    const filteredProducts = context.carProducts.filter(
+      (product) => product.id !== id
     );
-}
+    context.setCarProducts(filteredProducts);
+    context.setCount(context.count - 1);
+  };
+
+  const handleCheckout = () => {
+    const orderToAdd = {
+      products: context.carProducts,
+      totalProducts: context.carProducts.length,
+      totalPrice: totalPrice(context.carProducts),
+    };
+    
+    context.setOrder([...context.order, orderToAdd]);
+    context.setCarProducts([]);
+    context.setCount(0);
+    context.closeCheckoutSideMenu();
+   
+  };
+
+  return (
+    <aside
+      className={`${
+        context.isCheckoutSideMenuOpen ? "flex" : "hidden"
+      } checkout-side-menu flex flex-col fixed right-0 bg-white border-l shadow-2xl border-slate-100 shadow-gray-800`}
+    >
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">My Order</h2>
+        <div>
+          <XCircleIcon
+            className="w-6 h-6 cursor-pointer"
+            onClick={() => context.closeCheckoutSideMenu()}
+          />
+        </div>
+      </div>
+      <div className="cart-container flex-grow overflow-y-auto">
+        {context.carProducts.map((product) => (
+          <CartCard
+            key={product.id}
+            product={product}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+      <div className="inset-x-0 bottom-0 h-20 p-6 flex justify-between items-center shadow-sm">
+        <h1 className="font-medium text-xl">Total:</h1>
+        <h1 className="font-medium text-xl">
+          ${totalPrice(context.carProducts)}
+        </h1>
+      </div>
+      <div className="flex place-content-center p-6 h-20 ">
+        <Link to='/my-orders/last'>
+          <button
+            className="flex items-center justify-center rounded-md bg-slate-900  px-32 py-2.5 text-center text-sm font-medium cursor-pointer text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-300 h-10"
+            onClick={() => handleCheckout()}
+          >
+            <h1>Checkout</h1>
+          </button>
+        </Link>
+      </div>
+    </aside>
+  );
+};
 
 export default CheckoutSideMenu;
