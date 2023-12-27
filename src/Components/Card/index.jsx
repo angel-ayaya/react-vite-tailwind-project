31f6aad7a999572000956072a712e11576db0190
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-12-26 18:53:31 -0700

    Refactor Card component and add CheckCircleIcon

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,33 +1,53 @@
-import React, { useContext } from "react"
-import { ShoppingCartContext } from "../../Context"
+import React, { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
 const Card = (props) => {
   const { data } = props;
   const context = useContext(ShoppingCartContext);
-  
+
   const cartFunction = (event, product) => {
-    event.stopPropagation()
-    context.setCount(context.count + 1)
-    context.setCarProducts([...context.carProducts, product])
-    context.openCheckoutSideMenu()
-  }
+    event.stopPropagation();
+    context.setCount(context.count + 1);
+    context.setCarProducts([...context.carProducts, product]);
+    context.openCheckoutSideMenu();
+  };
 
   const showProduct = (product) => {
-    context.setProductToShow(product)
-    context.openProductDetail()
-    
-  }
+    context.setProductToShow(product);
+    context.openProductDetail();
+  };
+
+  const renderIcon = (product) => {
+    const isInCard =
+      context.carProducts.filter((product) => product.id === data.id).length >
+      0;
+
+    if (isInCard) {
+      return <CheckCircleIcon className="w-8 h-8 " />;
+    } else {
+      return (
+        <a
+          onClick={(event) => cartFunction(event, data)}
+          className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium cursor-pointer text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-300 max-h-8"
+        >
+          Add to cart
+        </a>
+      );
+    }
+  };
 
   return (
     <div className="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
       <div className="h-62 cursor-pointer" onClick={() => showProduct(data)}>
-        <a
-          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl min-h-60"
-
-        >
+        <a className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl min-h-60">
           <img
             className="object-cover hover:scale-105 h-62 hover:blur-sm hover:brightness-98 transition-all duration-500 ease-in-out"
-            src={data.images[0] ? data.images[0] : 'https://i.imgur.com/9LFjwpI.jpeg'}
+            src={
+              data.images[0]
+                ? data.images[0]
+                : "https://i.imgur.com/9LFjwpI.jpeg"
+            }
             alt="product image"
           />
           <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
@@ -44,18 +64,13 @@ const Card = (props) => {
           </a>
         </div>
         <div className="mt-2  flex items-center justify-between">
-          <p >
-            <span className="text-2xl font-bold text-slate-900">{`$ ` + data.price}</span>
+          <p>
+            <span className="text-2xl font-bold text-slate-900">
+              {`$ ` + data.price}
+            </span>
           </p>
-          <a
-            onClick={(event) => cartFunction(event, data)}
-            
-            className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium cursor-pointer text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-300"
-          >
-            Add to cart
-          </a>
+          {renderIcon(data)}
         </div>
-
       </div>
     </div>
   );
